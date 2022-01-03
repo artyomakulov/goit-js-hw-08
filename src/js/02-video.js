5f7b272ad523ead71cea5b4e68735660a78f1eb2
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2022-01-03 14:03:49 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,10 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 player.on('play', throttle(function () {
-    player.getCurrentTime().then(function (value) {
+    player.getCurrentTime(0).then(function (value) {
         localStorage.setItem('videoplayer-current-time', JSON.stringify(value))
     });}, 1000),);
 
-const startFrom = JSON.parse(localStorage.getItem('videoplayer-current-time', 0));
+const startFrom = JSON.parse(localStorage.getItem('videoplayer-current-time'));
 console.log(startFrom);
 player.setCurrentTime(startFrom)
